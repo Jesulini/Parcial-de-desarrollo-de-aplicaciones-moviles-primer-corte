01863b3b314e42767c33c664c5833a626a4388b2
Jesulini <jesulini14@gmail.com>
2025-08-30 04:38:44 -0500

    creacion de registro y un boton añadido en login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './login/login.page';
 import { HomePage } from './home/home.page';
+import { RegisterPage } from './register/register.page';  // Importa el componente standalone
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
   { path: 'home', component: HomePage },
+  { path: 'register', component: RegisterPage },  // Ruta para el componente standalone
 ];
