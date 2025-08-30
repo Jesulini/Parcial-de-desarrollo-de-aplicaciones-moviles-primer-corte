642a74851c9b6e47b2255f89179c7ef84a4b2003
Jesulini <jesulini14@gmail.com>
2025-08-30 06:17:16 -0500

    creacion de los componentes

---
+++ src/app/input-component/input-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-component',
+  templateUrl: './input-component.component.html',
+  styleUrls: ['./input-component.component.scss'],
+})
+export class InputComponentComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/select-component/select-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-component',
+  templateUrl: './select-component.component.html',
+  styleUrls: ['./select-component.component.scss'],
+})
+export class SelectComponentComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
