f1606b983f020187d47dcba9a36e60b0b72beb34
Jesulini <jesulini14@gmail.com>
2025-08-30 14:09:45 -0500

    reinicio total

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Storm TV',
-  webDir: 'www'
-};
-
-export default config;
